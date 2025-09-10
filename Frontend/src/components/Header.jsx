c6d0cc52a39c33c06d5868fbf5f035314727fd5d
Thomas <thomas.koob@gmx.net>
2025-09-10 16:51:23 +0200

    frontend Layout

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 
 function Header() {
   return (
     <header>
-      <nav>
+      <nav className="flex justify-between items-center p-4 bg-gray-800">
         <Link to="/">Home</Link>
         <Link to="/roster">My Roster</Link>
         <Link to="/battle">Battle</Link>
