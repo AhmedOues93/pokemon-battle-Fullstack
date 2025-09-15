098151c565611234875fa99929797da0033f014c
Marvin Wolff <m.w@nr1.io>
2025-09-15 15:19:13 +0200

    add backend URL

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,2 +1,3 @@
 export const API_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:3001/api";
+  /*import.meta.env.VITE_API_URL || "http://localhost:3001/api";*/
+  "https://pokemon-battle-9g5s.onrender.com/api";
